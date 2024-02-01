fca0ce5c58c5d64786d671527f83040d89ad06f9
Hugg25 <hugo.paulsson@hotmail.com>
2024-02-01 10:45:22 +0100

    ezpezy

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int resultat;
+
+bool TryParse(string s, out int result)
+{
+    try
+    {
+        result = int.Parse(s);
+        return true;
+    }
+
+    catch
+    {
+        result = 0;
+        return false;
+    }
+}
+
+if(!TryParse(Console.ReadLine(), out resultat))
+    Console.WriteLine("Skriv in en siffra");
+
+else
+    Console.WriteLine("Done!");
